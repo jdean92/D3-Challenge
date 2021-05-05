a4daf0f691475888c78c3889705c2e44f1c2219b
Jessica Dean <thething123789@yahoo.com>
2021-05-05 17:01:11 -0500

    d3

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -11,5 +11,18 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+d3.csv("assets/data/data.csv").then(function(CensusData) {
+  CensusData.forEach(function(data) {
+    data.age = +data.age;
+    data.smokes = +data.smokes;
+    // console.log(data);
+  });
