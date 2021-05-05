4f5ccca435497e38090d7db75d6aca51e1456cf0
Jessica Dean <thething123789@yahoo.com>
2021-05-05 17:03:07 -0500

    ysclae

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -30,6 +30,10 @@ d3.csv("assets/data/data.csv").then(function(CensusData) {
   .domain(d3.extent(CensusData, d => d.age))
   .range([0, width])
   .nice();
-
+  
+  const yScale = d3.scaleLinear()
+  .domain([6,d3.max(CensusData, d => d.smokes)])
+  .range([height, 0])
+  .nice();
 
 
